28db02674b99b57118edb64ccebc425551c14a57
zb0r0 <krystin52@gmail.com>
2024-04-25 18:20:34 +0200

    db do zrobienia

--- app.py
+++ app.py
@@ -1,15 +1,28 @@
-from flask import Flask, render_template, redirect, session, request
-import os, filetype
-import sqlalchemy
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
 
 app = Flask(__name__)
+app.app_context().push()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = '123secretkey'
+db = SQLAlchemy(app)
 
-
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False)
+    password = db.Column(db.String(80), nullable=False)
 
 @app.route('/')
-def home_page():  # put application's code here
+def home_page():
     return render_template("index.html")
+@app.route('/login')
+def login():
+    return render_template('login.html')
+@app.route('/register')
+def register():
+    return render_template('register.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
