cb4e859e0f1835ca001b5b46af4ce4a3814d673e
zb0r0 <krystin52@gmail.com>
2024-05-20 23:13:00 +0200

    db done:D

--- app.py
+++ app.py
@@ -3,26 +3,40 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 
 app = Flask(__name__)
-app.app_context().push()
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = '123secretkey'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mssql+pyodbc://@DESKTOP-DJ1UNI3/online_drive?driver=ODBC+Driver+17+for+SQL+Server&trusted_connection=yes'
 db = SQLAlchemy(app)
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), nullable=False)
-    password = db.Column(db.String(80), nullable=False)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    password = db.Column(db.String(50), nullable=False)
+    files = db.relationship('FileStorage', backref='user', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}')"
+
+class FileStorage(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    filename = db.Column(db.String(255), nullable=False)
+    filepath = db.Column(db.String(255), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.filename}')"
 
 @app.route('/')
 def home_page():
     return render_template("index.html")
+
 @app.route('/login')
 def login():
     return render_template('login.html')
+
 @app.route('/register')
 def register():
     return render_template('register.html')
-
 
 if __name__ == '__main__':
     app.run(debug=True)
